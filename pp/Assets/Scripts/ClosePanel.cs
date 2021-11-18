dc2214d501677a75910dc82166dfebecebb5bac9
yattrium <wolf.mikoto@gmail.com>
2021-11-18 17:20:23 +0300

    upp ar
    я хз делайте с ним че хотите

---
+++ pp/Assets/Scripts/ClosePanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosePanel : MonoBehaviour
+{
+    public GameObject OpenedPanel;
+
+    public void CloseP()
+    {
+        OpenedPanel.SetActive(false);
+    }
+
+}
